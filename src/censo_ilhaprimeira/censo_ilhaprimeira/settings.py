28c38149aa119235917a6bb1b06fe16c65195bb7
EstevaoMO <estevaomoraes48@gmail.com>
2025-06-13 09:50:03 -0300

    se essa não é a última, eu me mato

--- src/censo_ilhaprimeira/censo_ilhaprimeira/settings.py
+++ src/censo_ilhaprimeira/censo_ilhaprimeira/settings.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv 
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
